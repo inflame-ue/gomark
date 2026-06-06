150d82b2a2dab68ef37c42524386ad0d7f13ba5b
Ilia Oberemok <i.oberemok777@gmail.com>
2026-06-06 13:21:40 +0300

    refactor the handler's error handling pathways by extracting common functonality into the utils package, code readability increased significantly

--- internal/notes/handler.go
+++ internal/notes/handler.go
@@ -1,15 +1,19 @@
-package grammar
+package notes
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
-	"github.com/inflame-ue/gomark/internal/markdown"
+	"github.com/inflame-ue/gomark/internal/db"
 	"github.com/inflame-ue/gomark/internal/utils"
 )
 
-func HandleGrammarCheck(w http.ResponseWriter, r *http.Request) {
+type Note struct {
+	Title   string `json:"title"`
+	Content string `json:"text"`
+}
+
+func HandlePostNote(w http.ResponseWriter, r *http.Request, db *db.DB) {
 	if err := utils.RequireMethod(r, http.MethodPost); err != nil {
 		utils.WriteErrorAndLog(w, err, http.StatusMethodNotAllowed)
 	}
@@ -18,18 +22,14 @@ func HandleGrammarCheck(w http.ResponseWriter, r *http.Request) {
 		utils.WriteErrorAndLog(w, err, http.StatusBadRequest)
 	}
 
-	var markdown markdown.Markdown
-	if err := json.NewDecoder(r.Body).Decode(&markdown); err != nil {
+	var note Note
+	if err := json.NewDecoder(r.Body).Decode(&note); err != nil {
 		utils.WriteErrorAndLog(w, err, http.StatusBadRequest)
 	}
 
-	grammarIssues, err := checkGrammar(&markdown)
+	err := db.CreateNote(note.Title, note.Content)
 	if err != nil {
 		utils.WriteErrorAndLog(w, err, http.StatusInternalServerError)
 	}
 
-	err = utils.WriteJSON(w, grammarIssues)
-	if err != nil {
-		log.Print(err)
-	}
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,20 +3,40 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type errorResponse struct {
-	Status int `json:"status"`
+	Status  int    `json:"status"`
 	Message string `json:"message"`
 }
 
-func WriteError(w http.ResponseWriter, err error, status int) error {
-	errResp := errorResponse {
-		Status: status,
+func RequireMethod(r *http.Request, method string) error {
+	if r.Method != strings.ToUpper(method) {
+		return fmt.Errorf("expected the HTTP method to be %v, got %v instead", method, r.Method)
+	}
+	return nil
+}
+
+func RequireContentType(r *http.Request, contentType string) error {
+	reqContentType := r.Header.Get("Content-Type")
+	if reqContentType != strings.ToLower(contentType) {
+		return fmt.Errorf("expected the content type to be %v, got %v instead",
+			contentType, reqContentType)
+	}
+	return nil
+}
+
+func WriteErrorAndLog(w http.ResponseWriter, err error, status int) error {
+	log.Print(err.Error())
+
+	errResp := errorResponse{
+		Status:  status,
 		Message: err.Error(),
 	}
-	
+
 	data, err := json.Marshal(errResp)
 	if err != nil {
 		return fmt.Errorf("failed to marhal the error: %v", err)
